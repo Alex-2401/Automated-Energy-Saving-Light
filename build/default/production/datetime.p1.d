e13263d233706659bea4e57645726cffc5f69be8
Alex-2401 <alex.pittaras@gmail.com>
2026-01-18 23:41:44 +0000

    chore: remove tracked log files

--- build/default/production/datetime.p1.d
+++
@@ -1,7 +0,0 @@
-build/default/production/datetime.p1:  \
-datetime.c  \
-datetime.h  \
-LCD.h  \
-timers.h  \
-LEDarray.h  \
-interrupts.h 
--- build/default/production/main.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/production/main.p1:  \
-main.c  \
-LCD.h  \
-timers.h  \
-LEDarray.h  \
-interrupts.h  \
-comparator.h  \
-ADC.h  \
-datetime.h 
